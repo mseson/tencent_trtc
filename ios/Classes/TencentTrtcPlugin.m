24674ecfcdc528f14424eac63cb3348f3bfaca53
maixin <maixin@guoshengtianfeng.com>
2020-06-04 18:59:53 +0800

    添加ios代码

--- ios/Classes/TencentTrtcPlugin.m
+++ ios/Classes/TencentTrtcPlugin.m
@@ -1,4 +1,11 @@
 #import "TencentTrtcPlugin.h"
+#import <TXLiteAVSDK_TRTC/TRTCCloud.h>
+
+@interface TencentTrtcPlugin : NSObject<FlutterPlugin>
+
+@property (strong, nonatomic) TRTCCloud* trtcCloud;
+
+@end
 
 @implementation TencentTrtcPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -12,9 +19,19 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
+  }else if ([@"registerTrtc" isEqualToString:call.method]) {
+
+  }else if ([@"enterRoom" isEqualToString:call.method]) {
+
+  }else if ([@"exitRoom" isEqualToString:call.method]) {
+
+   }else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+- (void)registerTrtc() {
+    self.trtcCloud = [TRTCCloud ]
 }
 
 @end
