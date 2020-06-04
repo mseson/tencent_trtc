10e29ea5cf6f692fb65381fea5e864a0a991b994
maixin <maixin@guoshengtianfeng.com>
2020-06-04 17:01:50 +0800

    修改ios配置

--- ios/Classes/TencentTrtcPlugin.m
+++ ios/Classes/TencentTrtcPlugin.m
@@ -1,15 +1,20 @@
 #import "TencentTrtcPlugin.h"
-#if __has_include(<tencent_trtc/tencent_trtc-Swift.h>)
-#import <tencent_trtc/tencent_trtc-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "tencent_trtc-Swift.h"
-#endif
 
 @implementation TencentTrtcPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftTencentTrtcPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"tencent_trtc"
+            binaryMessenger:[registrar messenger]];
+  TencentTrtcPlugin* instance = [[TencentTrtcPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
